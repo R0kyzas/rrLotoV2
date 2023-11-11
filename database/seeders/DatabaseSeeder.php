1dcd796dc5e09464813a5a802c93ddd9646aca57
Sak0r <rokas.saudargis0@gmail.com>
2023-11-11 08:50:34 +0200

    updates

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,6 @@ class DatabaseSeeder extends Seeder
     {
         User::factory()->create();
         \App\Models\TicketPrice::factory(1)->create();
-        \App\Models\Discount::factory(5)->create();
+        // \App\Models\Discount::factory(5)->create();
     }
 }
